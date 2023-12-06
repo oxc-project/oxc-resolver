12bd93a45fe674c83b0a492a00be6d432f7cfd16
Boshen <boshenc@gmail.com>
2023-12-06 16:47:54 +0800

    add test for styled-components in real pnpm 8

--- tests/resolve_test.rs
+++ tests/resolve_test.rs
@@ -7,9 +7,31 @@ fn dir() -> PathBuf {
 }
 
 #[test]
-fn chinese_dir() {
+fn chinese() {
     let dir = dir();
     let specifier = "./fixtures/misc/中文/中文.js";
     let resolution = Resolver::new(ResolveOptions::default()).resolve(&dir, specifier);
-    assert_eq!(resolution.map(|r| r.into_path_buf()), Ok(dir.join("fixtures/misc/中文/中文.js")))
+    assert_eq!(resolution.map(|r| r.into_path_buf()), Ok(dir.join("fixtures/misc/中文/中文.js")));
+}
+
+#[test]
+fn styled_components() {
+    let dir = dir();
+    let path = dir.join("fixtures/pnpm8");
+    let specifier = "styled-components";
+
+    // cjs
+    let options =
+        ResolveOptions { alias_fields: vec![vec!["browser".into()]], ..ResolveOptions::default() };
+    let resolution = Resolver::new(options).resolve(&path, specifier);
+    assert_eq!(resolution.map(|r| r.into_path_buf()), Ok(path.join("node_modules/.pnpm/styled-components@6.1.1_react-dom@18.2.0_react@18.2.0/node_modules/styled-components/dist/styled-components.browser.cjs.js")));
+
+    // esm
+    let options = ResolveOptions {
+        alias_fields: vec![vec!["browser".into()]],
+        main_fields: vec!["module".into()],
+        ..ResolveOptions::default()
+    };
+    let resolution = Resolver::new(options).resolve(&path, specifier);
+    assert_eq!(resolution.map(|r| r.into_path_buf()), Ok(path.join("node_modules/.pnpm/styled-components@6.1.1_react-dom@18.2.0_react@18.2.0/node_modules/styled-components/dist/styled-components.browser.esm.js")));
 }
