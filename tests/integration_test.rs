3769c8292ca04631f73c7ae2afaae1ccbaa47cdc
Boshen <boshenc@gmail.com>
2023-12-06 15:45:57 +0800

    add integration_test

---
+++ tests/integration_test.rs
@@ -0,0 +1,43 @@
+use std::{env, path::PathBuf};
+
+use oxc_resolver::{Resolution, ResolveOptions, Resolver};
+
+fn dir() -> PathBuf {
+    env::current_dir().unwrap()
+}
+
+fn resolve(specifier: &str) -> Resolution {
+    let path = dir();
+    Resolver::new(ResolveOptions::default()).resolve(path, specifier).unwrap()
+}
+
+#[test]
+fn clone() {
+    let resolution = resolve("./tests/package.json");
+    assert_eq!(resolution.clone(), resolution);
+}
+
+#[test]
+fn debug() {
+    let resolution = resolve("./tests/package.json");
+    let s = format!("{:?}", resolution);
+    assert!(!s.is_empty());
+}
+
+#[test]
+fn eq() {
+    let resolution = resolve("./tests/package.json");
+    assert_eq!(resolution, resolution);
+}
+
+#[test]
+fn package_json() {
+    let resolution = resolve("./tests/package.json");
+    assert!(resolution.package_json().is_some());
+}
+
+#[test]
+fn clear_cache() {
+    let resolver = Resolver::new(ResolveOptions::default());
+    resolver.clear_cache(); // exists
+}
