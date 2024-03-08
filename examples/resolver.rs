208dcff7c43d3f31bc3fcd6e621aed129bc5ac15
Yunfei <i.heyunfei@gmail.com>
2024-03-08 13:35:49 +0800

    Cannot resolve vue

--- examples/resolver.rs
+++ examples/resolver.rs
@@ -4,16 +4,15 @@ use oxc_resolver::{AliasValue, ResolveOptions, Resolver};
 
 fn main() {
     // Path to directory, must be in absolute path.
-    let path = env::args().nth(1).expect("path");
-    let specifier = env::args().nth(2).expect("specifier");
+    let path = ".";
+    let specifier = "vue";
     let path = PathBuf::from(path).canonicalize().unwrap();
 
     println!("path: {path:?}");
     println!("request: {specifier}");
 
     let options = ResolveOptions {
-        alias_fields: vec![vec!["browser".into()]],
-        alias: vec![("asdf".into(), vec![AliasValue::Path("./test.js".into())])],
+        alias_fields: vec![vec!["exports".into()]],
         ..ResolveOptions::default()
     };
 
