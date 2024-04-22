0224a85dd2c554ae7d0cd2fd7fd86ebc15a166cb
Boshen <boshenc@gmail.com>
2024-04-23 01:57:03 +0800

    fix: incorrect resolution when using shared resolvers with different `main_fields`

--- src/tests/main_field.rs
+++ src/tests/main_field.rs
@@ -6,19 +6,19 @@ use crate::{ResolveOptions, Resolver};
 fn test() {
     let f = super::fixture().join("restrictions");
 
-    let resolver = Resolver::new(ResolveOptions {
+    let resolver1 = Resolver::new(ResolveOptions {
         main_fields: vec!["style".into()],
         ..ResolveOptions::default()
     });
 
-    let resolution = resolver.resolve(&f, "pck2").map(|r| r.full_path());
+    let resolution = resolver1.resolve(&f, "pck2").map(|r| r.full_path());
     assert_eq!(resolution, Ok(f.join("node_modules/pck2/index.css")));
 
-    let resolver = Resolver::new(ResolveOptions {
+    let resolver2 = resolver1.clone_with_options(ResolveOptions {
         main_fields: vec!["module".into(), "main".into()],
         ..ResolveOptions::default()
     });
 
-    let resolution = resolver.resolve(&f, "pck2").map(|r| r.full_path());
+    let resolution = resolver2.resolve(&f, "pck2").map(|r| r.full_path());
     assert_eq!(resolution, Ok(f.join("node_modules/pck2/module.js")));
 }
