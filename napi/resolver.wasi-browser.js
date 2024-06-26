3f81485673855323885949b224d3701b04af7f4b
LongYinan <lynweklm@gmail.com>
2024-06-26 12:00:48 +0800

    chore: re-enable the wasi build

--- napi/resolver.wasi-browser.js
+++ napi/resolver.wasi-browser.js
@@ -2,27 +2,26 @@ import {
   instantiateNapiModuleSync as __emnapiInstantiateNapiModuleSync,
   getDefaultContext as __emnapiGetDefaultContext,
   WASI as __WASI,
+  createOnMessage as __wasmCreateOnMessageForFsProxy,
 } from '@napi-rs/wasm-runtime'
-import { Volume as __Volume, createFsFromVolume as __createFsFromVolume } from '@napi-rs/wasm-runtime/fs'
-
+import { memfs } from '@napi-rs/wasm-runtime/fs'
 import __wasmUrl from './resolver.wasm32-wasi.wasm?url'
 
-const __fs = __createFsFromVolume(
-  __Volume.fromJSON({
-    '/': null,
-  }),
-)
+export const { fs: __fs, vol: __volume } = memfs()
 
 const __wasi = new __WASI({
   version: 'preview1',
   fs: __fs,
+  preopens: {
+    '/': '/',
+  }
 })
 
 const __emnapiContext = __emnapiGetDefaultContext()
 
 const __sharedMemory = new WebAssembly.Memory({
-  initial: 1024,
-  maximum: 10240,
+  initial: 4000,
+  maximum: 65536,
   shared: true,
 })
 
@@ -37,9 +36,13 @@ const {
   asyncWorkPoolSize: 4,
   wasi: __wasi,
   onCreateWorker() {
-    return new Worker(new URL('./wasi-worker-browser.mjs', import.meta.url), {
+    const worker = new Worker(new URL('./wasi-worker-browser.mjs', import.meta.url), {
       type: 'module',
     })
+    
+    worker.addEventListener('message', __wasmCreateOnMessageForFsProxy(__fs))
+
+    return worker
   },
   overwriteImports(importObject) {
     importObject.env = {
@@ -63,7 +66,7 @@ function __napi_rs_initialize_modules(__napiInstance) {
   __napiInstance.exports['__napi_register__ResolveResult_struct_4']?.()
   __napiInstance.exports['__napi_register__sync_5']?.()
   __napiInstance.exports['__napi_register__ResolverFactory_struct_6']?.()
-  __napiInstance.exports['__napi_register__ResolverFactory_impl_12']?.()
+  __napiInstance.exports['__napi_register__ResolverFactory_impl_13']?.()
 }
 export const ResolverFactory = __napiModule.exports.ResolverFactory
 export const EnforceExtension = __napiModule.exports.EnforceExtension
