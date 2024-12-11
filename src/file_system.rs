17441f834695935ffd3efc1ab167b3462d15e5a4
Boshen <boshenc@gmail.com>
2024-12-11 23:58:07 +0800

    feat!: replace `FileSystem::canonicalize` with `FileSystem::read_link` (#331)

--- src/file_system.rs
+++ src/file_system.rs
@@ -7,11 +7,6 @@ use cfg_if::cfg_if;
 #[cfg(feature = "yarn_pnp")]
 use pnp::fs::{LruZipCache, VPath, VPathInfo, ZipCache};
 
-#[cfg(windows)]
-const UNC_PATH_PREFIX: &[u8] = b"\\\\?\\UNC\\";
-#[cfg(windows)]
-const LONG_PATH_PREFIX: &[u8] = b"\\\\?\\";
-
 /// File System abstraction used for `ResolverGeneric`
 pub trait FileSystem: Send + Sync {
     /// See [std::fs::read_to_string]
@@ -49,17 +44,12 @@ pub trait FileSystem: Send + Sync {
     /// napi env.
     fn symlink_metadata(&self, path: &Path) -> io::Result<FileMetadata>;
 
-    /// See [std::fs::canonicalize]
+    /// Returns the resolution of a symbolic link.
     ///
     /// # Errors
     ///
     /// See [std::fs::read_link]
-    /// ## Warning
-    /// Use `&Path` instead of a generic `P: AsRef<Path>` here,
-    /// because object safety requirements, it is especially useful, when
-    /// you want to store multiple `dyn FileSystem` in a `Vec` or use a `ResolverGeneric<Fs>` in
-    /// napi env.
-    fn canonicalize(&self, path: &Path) -> io::Result<PathBuf>;
+    fn read_link(&self, path: &Path) -> io::Result<PathBuf>;
 }
 
 /// Metadata information about a file
@@ -111,18 +101,70 @@ impl Default for FileSystemOs {
     }
 }
 
-fn read_to_string(path: &Path) -> io::Result<String> {
-    // `simdutf8` is faster than `std::str::from_utf8` which `fs::read_to_string` uses internally
-    let bytes = std::fs::read(path)?;
-    if simdutf8::basic::from_utf8(&bytes).is_err() {
-        // Same error as `fs::read_to_string` produces (`io::Error::INVALID_UTF8`)
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidData,
-            "stream did not contain valid UTF-8",
-        ));
+impl FileSystemOs {
+    /// # Errors
+    ///
+    /// See [std::fs::read_to_string]
+    pub fn read_to_string(path: &Path) -> io::Result<String> {
+        // `simdutf8` is faster than `std::str::from_utf8` which `fs::read_to_string` uses internally
+        let bytes = std::fs::read(path)?;
+        if simdutf8::basic::from_utf8(&bytes).is_err() {
+            // Same error as `fs::read_to_string` produces (`io::Error::INVALID_UTF8`)
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "stream did not contain valid UTF-8",
+            ));
+        }
+        // SAFETY: `simdutf8` has ensured it's a valid UTF-8 string
+        Ok(unsafe { String::from_utf8_unchecked(bytes) })
     }
-    // SAFETY: `simdutf8` has ensured it's a valid UTF-8 string
-    Ok(unsafe { String::from_utf8_unchecked(bytes) })
+
+    /// # Errors
+    ///
+    /// See [std::fs::metadata]
+    #[inline]
+    pub fn metadata(path: &Path) -> io::Result<FileMetadata> {
+        fs::metadata(path).map(FileMetadata::from)
+    }
+
+    /// # Errors
+    ///
+    /// See [std::fs::symlink_metadata]
+    #[inline]
+    pub fn symlink_metadata(path: &Path) -> io::Result<FileMetadata> {
+        fs::symlink_metadata(path).map(FileMetadata::from)
+    }
+
+    /// # Errors
+    ///
+    /// See [std::fs::read_link]
+    #[inline]
+    pub fn read_link(path: &Path) -> io::Result<PathBuf> {
+        let path = fs::read_link(path)?;
+        cfg_if! {
+            if #[cfg(windows)] {
+                Ok(Self::strip_windows_prefix(path))
+            } else {
+                Ok(path)
+            }
+        }
+    }
+
+    pub fn strip_windows_prefix<P: AsRef<Path>>(path: P) -> PathBuf {
+        const UNC_PATH_PREFIX: &[u8] = b"\\\\?\\UNC\\";
+        const LONG_PATH_PREFIX: &[u8] = b"\\\\?\\";
+        let path_bytes = path.as_ref().as_os_str().as_encoded_bytes();
+        path_bytes
+            .strip_prefix(UNC_PATH_PREFIX)
+            .or_else(|| path_bytes.strip_prefix(LONG_PATH_PREFIX))
+            .map_or_else(
+                || path.as_ref().to_path_buf(),
+                |p| {
+                    // SAFETY: `as_encoded_bytes` ensures `p` is valid path bytes
+                    unsafe { PathBuf::from(std::ffi::OsStr::from_encoded_bytes_unchecked(p)) }
+                },
+            )
+    }
 }
 
 impl FileSystem for FileSystemOs {
@@ -133,11 +175,11 @@ impl FileSystem for FileSystemOs {
                     VPath::Zip(info) => {
                         self.pnp_lru.read_to_string(info.physical_base_path(), info.zip_path)
                     }
-                    VPath::Virtual(info) => read_to_string(&info.physical_base_path()),
-                    VPath::Native(path) => read_to_string(&path),
+                    VPath::Virtual(info) => Self::read_to_string(&info.physical_base_path()),
+                    VPath::Native(path) => Self::read_to_string(&path),
                 }
             } else {
-                read_to_string(path)
+                Self::read_to_string(path)
             }
         }
     }
@@ -151,30 +193,30 @@ impl FileSystem for FileSystemOs {
                         .file_type(info.physical_base_path(), info.zip_path)
                         .map(FileMetadata::from),
                     VPath::Virtual(info) => {
-                        fs::metadata(info.physical_base_path()).map(FileMetadata::from)
+                        Self::metadata(&info.physical_base_path()).map(FileMetadata::from)
                     }
-                    VPath::Native(path) => fs::metadata(path).map(FileMetadata::from),
+                    VPath::Native(path) => Self::metadata(&path).map(FileMetadata::from),
                 }
             } else {
-                fs::metadata(path).map(FileMetadata::from)
+                Self::metadata(path).map(FileMetadata::from)
             }
         }
     }
 
     fn symlink_metadata(&self, path: &Path) -> io::Result<FileMetadata> {
-        fs::symlink_metadata(path).map(FileMetadata::from)
+        Self::symlink_metadata(path).map(FileMetadata::from)
     }
 
-    fn canonicalize(&self, path: &Path) -> io::Result<PathBuf> {
+    fn read_link(&self, path: &Path) -> io::Result<PathBuf> {
         cfg_if! {
             if #[cfg(feature = "yarn_pnp")] {
                 match VPath::from(path)? {
-                    VPath::Zip(info) => fast_canonicalize(info.physical_base_path().join(info.zip_path)),
-                    VPath::Virtual(info) => fast_canonicalize(info.physical_base_path()),
-                    VPath::Native(path) => fast_canonicalize(path),
+                    VPath::Zip(info) => Self::read_link(&info.physical_base_path().join(info.zip_path)),
+                    VPath::Virtual(info) => Self::read_link(&info.physical_base_path()),
+                    VPath::Native(path) => Self::read_link(&path),
                 }
             } else {
-                fast_canonicalize(path)
+                Self::read_link(path)
             }
         }
     }
@@ -188,78 +230,4 @@ fn metadata() {
         "FileMetadata { is_file: true, is_dir: true, is_symlink: true }"
     );
     let _ = meta;
-}
-
-#[inline]
-fn fast_canonicalize<P: AsRef<Path>>(path: P) -> io::Result<PathBuf> {
-    #[cfg(windows)]
-    {
-        // fs::canonicalize was faster on Windows (https://github.com/oxc-project/oxc-resolver/pull/306)
-        Ok(node_compatible_raw_canonicalize(fs::canonicalize(path)?))
-    }
-    #[cfg(not(windows))]
-    {
-        fast_canonicalize_non_windows(path.as_ref().to_path_buf())
-    }
-}
-
-#[inline]
-#[cfg(not(windows))]
-// This is A faster fs::canonicalize implementation by reducing the number of syscalls
-fn fast_canonicalize_non_windows(path: PathBuf) -> io::Result<PathBuf> {
-    use std::path::Component;
-    let mut path_buf = path;
-
-    loop {
-        let link = fs::read_link(&path_buf)?;
-        path_buf.pop();
-        if fs::symlink_metadata(&path_buf)?.is_symlink() {
-            path_buf = fast_canonicalize(path_buf)?;
-        }
-        for component in link.components() {
-            match component {
-                Component::ParentDir => {
-                    path_buf.pop();
-                }
-                Component::Normal(seg) => {
-                    #[cfg(target_family = "wasm")]
-                    {
-                        // Need to trim the extra \0 introduces by https://github.com/nodejs/uvwasi/issues/262
-                        path_buf.push(seg.to_string_lossy().trim_end_matches('\0'));
-                    }
-                    #[cfg(not(target_family = "wasm"))]
-                    {
-                        path_buf.push(seg);
-                    }
-                }
-                Component::RootDir => {
-                    path_buf = PathBuf::from("/");
-                }
-                Component::CurDir | Component::Prefix(_) => {}
-            }
-
-            if fs::symlink_metadata(&path_buf)?.is_symlink() {
-                path_buf = fast_canonicalize(path_buf)?;
-            }
-        }
-        if !fs::symlink_metadata(&path_buf)?.is_symlink() {
-            break;
-        }
-    }
-    Ok(path_buf)
-}
-
-#[cfg(windows)]
-fn node_compatible_raw_canonicalize<P: AsRef<Path>>(path: P) -> PathBuf {
-    let path_bytes = path.as_ref().as_os_str().as_encoded_bytes();
-    path_bytes
-        .strip_prefix(UNC_PATH_PREFIX)
-        .or_else(|| path_bytes.strip_prefix(LONG_PATH_PREFIX))
-        .map_or_else(
-            || path.as_ref().to_path_buf(),
-            |p| {
-                // SAFETY: `as_encoded_bytes` ensures `p` is valid path bytes
-                unsafe { PathBuf::from(std::ffi::OsStr::from_encoded_bytes_unchecked(p)) }
-            },
-        )
 }
