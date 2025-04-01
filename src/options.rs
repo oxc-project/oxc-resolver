b1f01a9ad1d7a6d6e4b4098e9da940943793f89b
Cong-Cong Pan <dacongsama@live.com>
2025-04-01 15:55:58 +0800

    feat: support pass closure to restriction
    back port https://github.com/web-infra-dev/rspack-resolver/pull/60

--- src/options.rs
+++ src/options.rs
@@ -1,6 +1,7 @@
 use std::{
     fmt,
     path::{Path, PathBuf},
+    sync::Arc,
 };
 
 /// Module Resolution Options
@@ -456,10 +457,19 @@ where
 }
 
 /// Value for [ResolveOptions::restrictions]
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub enum Restriction {
     Path(PathBuf),
-    RegExp(String),
+    Fn(Arc<dyn Fn(&Path) -> bool + Sync + Send>),
+}
+
+impl std::fmt::Debug for Restriction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Path(path) => write!(f, "Path(\"{}\")", path.display()),
+            Self::Fn(_) => write!(f, "Fn(<function>)"),
+        }
+    }
 }
 
 /// Tsconfig Options for [ResolveOptions::tsconfig]
--- src/tests/restrictions.rs
+++ src/tests/restrictions.rs
@@ -1,16 +1,48 @@
 //! <https://github.com/webpack/enhanced-resolve/blob/main/test/restrictions.test.js>
 
+use std::sync::Arc;
+
+use regex::Regex;
+
 use crate::{ResolveError, ResolveOptions, Resolver, Restriction};
 
-// TODO: regex
-// * should respect RegExp restriction
-// * should try to find alternative #1
-// * should try to find alternative #2
-// * should try to find alternative #3
-
-// should respect string restriction
 #[test]
-fn restriction1() {
+fn should_respect_regexp_restriction() {
+    let f = super::fixture().join("restrictions");
+
+    let re = Regex::new(r"\.(sass|scss|css)$").unwrap();
+    let resolver1 = Resolver::new(ResolveOptions {
+        extensions: vec![".js".into()],
+        restrictions: vec![Restriction::Fn(Arc::new(move |path| {
+            path.as_os_str().to_str().is_some_and(|s| re.is_match(s))
+        }))],
+        ..ResolveOptions::default()
+    });
+
+    let resolution = resolver1.resolve(&f, "pck1").map(|r| r.full_path());
+    assert_eq!(resolution, Err(ResolveError::NotFound("pck1".to_string())));
+}
+
+#[test]
+fn should_try_to_find_alternative_1() {
+    let f = super::fixture().join("restrictions");
+
+    let re = Regex::new(r"\.(sass|scss|css)$").unwrap();
+    let resolver1 = Resolver::new(ResolveOptions {
+        extensions: vec![".js".into(), ".css".into()],
+        main_files: vec!["index".into()],
+        restrictions: vec![Restriction::Fn(Arc::new(move |path| {
+            path.as_os_str().to_str().is_some_and(|s| re.is_match(s))
+        }))],
+        ..ResolveOptions::default()
+    });
+
+    let resolution = resolver1.resolve(&f, "pck1").map(|r| r.full_path());
+    assert_eq!(resolution, Ok(f.join("node_modules/pck1/index.css")));
+}
+
+#[test]
+fn should_respect_string_restriction() {
     let fixture = super::fixture();
     let f = fixture.join("restrictions");
 
@@ -21,5 +53,41 @@ fn restriction1() {
     });
 
     let resolution = resolver.resolve(&f, "pck2");
-    assert_eq!(resolution, Err(ResolveError::Restriction(fixture.join("c.js"), f)));
+    assert_eq!(resolution, Err(ResolveError::NotFound("pck2".to_string())));
+}
+
+#[test]
+fn should_try_to_find_alternative_2() {
+    let f = super::fixture().join("restrictions");
+
+    let re = Regex::new(r"\.(sass|scss|css)$").unwrap();
+    let resolver1 = Resolver::new(ResolveOptions {
+        extensions: vec![".js".into(), ".css".into()],
+        main_fields: vec!["main".into(), "style".into()],
+        restrictions: vec![Restriction::Fn(Arc::new(move |path| {
+            path.as_os_str().to_str().is_some_and(|s| re.is_match(s))
+        }))],
+        ..ResolveOptions::default()
+    });
+
+    let resolution = resolver1.resolve(&f, "pck2").map(|r| r.full_path());
+    assert_eq!(resolution, Ok(f.join("node_modules/pck2/index.css")));
+}
+
+#[test]
+fn should_try_to_find_alternative_3() {
+    let f = super::fixture().join("restrictions");
+
+    let re = Regex::new(r"\.(sass|scss|css)$").unwrap();
+    let resolver1 = Resolver::new(ResolveOptions {
+        extensions: vec![".js".into()],
+        main_fields: vec!["main".into(), "module".into(), "style".into()],
+        restrictions: vec![Restriction::Fn(Arc::new(move |path| {
+            path.as_os_str().to_str().is_some_and(|s| re.is_match(s))
+        }))],
+        ..ResolveOptions::default()
+    });
+
+    let resolution = resolver1.resolve(&f, "pck2").map(|r| r.full_path());
+    assert_eq!(resolution, Ok(f.join("node_modules/pck2/index.css")));
 }
