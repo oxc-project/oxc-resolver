f4c52c61b623dce8368e18420e8cc195d1cd5622
Boshen <boshenc@gmail.com>
2025-09-23 18:32:14 +0800

    test: add memory leak test

---
+++ src/tests/memory_leak.rs
@@ -0,0 +1,24 @@
+use std::sync::Arc;
+
+use crate::Resolver;
+
+/// Test to prove memory leak in `CachedPath` Arc cycles
+#[test]
+fn test_memory_leak_arc_cycles() {
+    let f = super::fixture_root().join("misc");
+
+    let resolver = Resolver::default();
+
+    let path = resolver.cache.value(&f);
+
+    resolver.resolve(&f, "package-json-nested").unwrap();
+
+    // Populated cache - path is now owned in multiple places.
+    assert_eq!(Arc::strong_count(&path.0), 4);
+
+    // Drop the resolver.
+    drop(resolver);
+
+    // All Arcs must be dropped, leaving the original count of 1.
+    assert_eq!(Arc::strong_count(&path.0), 1);
+}
